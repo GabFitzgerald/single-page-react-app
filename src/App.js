4eb3f129222a867700329a076537db85930ca3cc
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-01 14:32:26 +1000

    lesson ended

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import HomeView from './HomeView'
 import CategorySelectionView from './CategorySelectionView'
 import NewEntryView from './NewEntryView'
 
 const App = () => {
-	const [page, setPage] = useState("home")
-
+	const categories = ["development", "movies", "philosophy", "other"]
 	return (
 		<div>
-			<nav>
-				<button onClick ={() => setPage("home")}>Home</button>
-				<button onClick ={() => setPage("category")}>Category</button>
-				<button onClick ={() => setPage("newEntry")}>New Entry</button>
-			</nav>
-			{page == "home" && <HomeView />}
-			{page == "category" && <CategorySelectionView />}
-			{page == "newEntry" && <NewEntryView />}
+			<BrowserRouter>
+				<nav>
+					<Link to="/">Home</Link>
+					<Link to="/category">Category</Link>
+					<Link to="/entry">New Entry</Link>
+				</nav>
+				<Route exact path="/" component={HomeView} />
+				<Route exact path="/category" render={props => <CategorySelectionView {...props} categories={categories} />} />
+				<Route exact path="/entry" render={props => <NewEntryView {...props} categories={categories} />} />
+				<Route exact path="entry/new/:cat_id" render={props => <NewEntryView {...props} categories={categories} />} />
+			</BrowserRouter>
 		</div>
 	)
 }
--- src/NewEntryView.js
+++ src/NewEntryView.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function NewEntryView() {
+export default function NewEntryView(props) {
+    const { cat_id } = props.match.params
+    const { categories } = props
     return (
         <div>
             <h1>New Entry View</h1>
+            <h4>Category: {categories[cat_id]} </h4>
         </div>
     )
 }
