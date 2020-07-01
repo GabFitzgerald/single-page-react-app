5bde17f0229f3087b564d57f3a833dcd843c376c
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-01 11:46:15 +1000

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HomeView from './HomeView'
 import CategorySelectionView from './CategorySelectionView'
 import NewEntryView from './NewEntryView'
 
 const App = () => {
+	const [page, setPage] = useState("home")
+
 	return (
 		<div>
-			<HomeView />
-			<CategorySelectionView />
-			<NewEntryView />
+			<nav>
+				<button onClick ={() => setPage("home")}>Home</button>
+				<button onClick ={() => setPage("category")}>Category</button>
+				<button onClick ={() => setPage("newEntry")}>New Entry</button>
+			</nav>
+			{page == "home" && <HomeView />}
+			{page == "category" && <CategorySelectionView />}
+			{page == "newEntry" && <NewEntryView />}
 		</div>
 	)
 }
